95097a215da46ed099942e33b8cd6aa3acbe9b18
Preston Watson <preston.watson89@gmail.com>
2016-05-21 14:29:27 -0500

    added the relations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  has_many :tasks
+  belongs_to :owner, class_name: "User"
+  has_many :users, through: :tasks, class_name: "User"
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :user
 
   state_machine :state, initial: :open do
 
