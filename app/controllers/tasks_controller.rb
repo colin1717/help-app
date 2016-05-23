27c4192d96eeb3a718b6a7190ce31729ef325aa8
Colin Hancock <colin1717@mac.com>
2016-05-23 09:30:16 -0500

    added authenticateuser to projects and tasks controllers"

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /tasks
   # GET /tasks.json
