9a292668506e3d7d7f3672405f722e3ace0e84be
Christopher Porter <snooper90@gmail.com>
2016-05-26 14:54:25 -0500

    added active page to navbar and links to correct sites

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+ready = function(){
+  $('ul.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
+}
+$(document).on('page:load ready', ready);
